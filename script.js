2416ab8909a649e538f655e90e7457cf5759cfb9
Khoa Diệp <143460572+Misax10@users.noreply.github.com>
2024-04-02 16:57:21 -0700

    working on project

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
-
-// Typing effect for message
-const message = "Here's to another year of wonderful adventures together!";
+const message =
+  "Wishing you all the happiness your heart can hold... Here's to another year of wonderful adventures together!";
 let index = 0;
+const typingSpeed = 100; // Adjust the typing speed in milliseconds
 
 function typeMessage() {
-    if (index < message.length) {
-        document.getElementById("typed-message").innerHTML += message.charAt(index);
-        index++;
-        setTimeout(typeMessage, 150); // Speed of typing
-    }
+  if (index < message.length) {
+    document.getElementById("typed-message").innerHTML += message.charAt(index);
+    index++;
+    setTimeout(typeMessage, typingSpeed);
+  }
 }
 
-window.onload = function() {
-    typeMessage();
-}
+window.onload = function () {
+  typeMessage();
+};
