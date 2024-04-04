23aea8424720d0afadf920c21ac3c590aa369680
Khoa Diệp <143460572+Misax10@users.noreply.github.com>
2024-04-03 20:26:27 -0700

    update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const message =`
+const message = `
 Happy Birthday ny iu dấu của anh. Mới đây mà đã gần một năm rồi nhanh quá à, 
 cảm ơn người iu luôn ở bên quan tâm, chăm sóc lo lắng cho anh mỗi ngày. Tuổi mới anh chúc ny có tất cả và chúc 2 đứa mình
 sẽ luôn hạnh phúc, mau về chung 1 nhà nha em iu
-`
+`;
 let index = 0;
 const typingSpeed = 50; // Adjust the typing speed in milliseconds
 
@@ -29,12 +29,14 @@ document.getElementById("revealButton").addEventListener("click", function () {
   var typedMessage = document.getElementById("typed-message");
   var imageGrid = document.querySelector(".image-grid");
 
-  // Remove the 'hidden' class and add the 'fade-in' class
-  typedMessage.classList.remove("hidden");
-  typedMessage.classList.add("fade-in");
+  // Use setTimeout to allow the display change to take effect
+  setTimeout(() => {
+    typedMessage.classList.remove("hidden");
+    typedMessage.classList.add("fade-in");
 
-  imageGrid.classList.remove("hidden");
-  imageGrid.classList.add("fade-in");
+    imageGrid.classList.remove("hidden");
+    imageGrid.classList.add("fade-in");
+  }, 10); // Small delay
 
   // Optionally hide the button after click
   this.style.display = "none";
@@ -42,22 +44,24 @@ document.getElementById("revealButton").addEventListener("click", function () {
   // If you want to initiate the typing effect upon reveal, call the typing function here
   typeMessage(); // Uncomment if you want the typing effect to start now
 });
-document.addEventListener('DOMContentLoaded', function() {
-  var backgroundMusic = document.getElementById('backgroundMusic');
-  var playPauseButton = document.querySelector('.play-pause');
-  var volumeControl = document.querySelector('.volume-control');
+document.addEventListener("DOMContentLoaded", function () {
+  var backgroundMusic = document.getElementById("backgroundMusic");
+  var playPauseButton = document.querySelector(".play-pause");
+  var volumeControl = document.querySelector(".volume-control");
 
-  playPauseButton.addEventListener('click', function() {
-      if (backgroundMusic.paused) {
-          backgroundMusic.play();
-          playPauseButton.textContent = 'Pause';
-      } else {
-          backgroundMusic.pause();
-          playPauseButton.textContent = 'Play';
-      }
+  playPauseButton.addEventListener("click", function () {
+    if (backgroundMusic.paused) {
+      backgroundMusic.play();
+      playPauseButton.textContent = "Pause";
+    } else {
+      backgroundMusic.pause();
+      playPauseButton.textContent = "Play";
+    }
   });
 
-  volumeControl.addEventListener('input', function() {
-      backgroundMusic.volume = this.value;
+  volumeControl.addEventListener("input", function () {
+    backgroundMusic.volume = this.value;
   });
 });
+
+
